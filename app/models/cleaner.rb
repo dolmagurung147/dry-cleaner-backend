7d2114a5d71080e54f57730355d340309420c348
Dolma Gurung <dolmagurung147@gmail.com>
2019-06-19 15:26:37 -0400

    seed data

--- app/models/cleaner.rb
+++ app/models/cleaner.rb
@@ -1,2 +1,5 @@
 class Cleaner < ApplicationRecord
+
+  has_many :laundries
+  has_many :users, through: :laundries
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+
+u1 = User.create(name: "Elizabeth", address: "Brooklyn", phone: "9871234567", email: "ekos26@gmail.com")
+
+c1 = Cleaner.create(name: "Elizabeth's Laundromat", location: "Brooklyn", phone: "1239875678", email: "elizlaundromat@gmail.com")
+
+Laundry.create(price: "$23", user_id: u1.id, cleaner_id: c1.id)
